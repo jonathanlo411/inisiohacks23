0a8336bcf9f466b9309bc9d689ed7cdc7832d77f
Bryton Lee <brl013@ucsd.edu>
2023-10-20 16:59:27 -0700

    feat: got mongodb to work

--- server.py
+++ server.py
@@ -1,11 +1,27 @@
 
 from flask import Flask, request, render_template
+from flask_pymongo import PyMongo
+import os
+from dotenv import load_dotenv
 
 
 # Setup
+load_dotenv()
 app = Flask(__name__)
+app.config["MONGO_URI"] = os.getenv('MONGO_URI')
+MONGO_URI = os.getenv('MONGO_URI')
+
+# Setup MongoDB
+mongo = PyMongo(app)
 
 # Page Render
 @app.route('/', methods=['GET'])
 def landing_page():
-    return render_template('index.html')
+    #print(mongo.db)
+    temp = mongo.db.sample
+    awa= temp.find()
+    #print(len(awa), flush=True)
+    #for i in awa:
+    #print(awa[0]['user'], flush=True)
+    
+    return render_template('index.html', context={"MONGO_URI": list(awa)})
