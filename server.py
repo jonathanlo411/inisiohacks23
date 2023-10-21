fbeaf3d2316610cfdbc1922492d1676c62c434f7
Jonathan Lo <jonathanlo411@gmail.com>
2023-10-20 17:41:15 -0700

    refactor: shift file structure to organize static files

--- server.py
+++ server.py
@@ -9,3 +9,7 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def landing_page():
     return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST)'])
+def login_page():
+    return render_template('login.html')
