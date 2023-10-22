316339817341ded46f5a931f26bc88b9ebf1fc70
Jonathan Lo <jonathanlo411@gmail.com>
2023-10-21 20:57:56 -0700

    feat: add songlink and votes frontend

--- static/js/modal.js
+++ static/js/modal.js
@@ -1,3 +1,14 @@
 
-let modalId = document.getElementsByClassName('modal')[0].id
-console.log(modalId)
+async function songVote(element, upvote) {
+    let data = await fetch('/api/votes', {
+        method: 'POST',
+        body: JSON.stringify({
+            musicID: element.dataset.id,
+            upvote: upvote
+        }),
+        headers: { "Content-Type": "application/json" }
+    })
+    console.log(data)
+}
+
+function playSong(id) { location.href = `/play?id=${id}` }
